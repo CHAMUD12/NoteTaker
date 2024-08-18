efbd89620f3b3204393280cfc0676dc6bc04f6db
CHAMUD12 <chamudgamage@gmail.com>
2024-08-18 15:47:05 +0530

    save note

--- src/main/java/org/example/notetaker/controller/NoteController.java
+++ src/main/java/org/example/notetaker/controller/NoteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/note")
+@RequestMapping("/api/v1/notes")
 public class NoteController {
     //Todo: SAVE a note
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
