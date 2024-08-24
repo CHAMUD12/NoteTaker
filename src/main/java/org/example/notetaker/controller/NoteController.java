beb45ef4705d075f5d66415d1251feb77a09677a
CHAMUD12 <chamudgamage@gmail.com>
2024-08-24 23:06:11 +0530

    check save,getall,search,update,delete is working

--- src/main/java/org/example/notetaker/controller/NoteController.java
+++ src/main/java/org/example/notetaker/controller/NoteController.java
@@ -19,7 +19,7 @@ public class NoteController {
     private final NoteBO noteBO;
 
     //Todo: SAVE a note
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)   // http://localhost:8080/NoteTaker_war_exploded/api/v1/notes
     public ResponseEntity<String> createNote(@RequestBody NoteDTO note){
         //Todo: Handle with BO
         var saveData = noteBO.saveNote(note);
@@ -27,14 +27,14 @@ public class NoteController {
     }
 
     //Todo: GetAll a note
-    @GetMapping(value = "allnotes", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "allnotes", produces = MediaType.APPLICATION_JSON_VALUE)    // http://localhost:8080/NoteTaker_war_exploded/api/v1/notes/allnotes
     public List<NoteDTO> getAllNotes(){
         System.out.println("Get all notes");
         return null;
     }
 
     //Todo: SEARCH a note
-    @GetMapping(value = "/{noteId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{noteId}", produces = MediaType.APPLICATION_JSON_VALUE)   // http://localhost:8080/NoteTaker_war_exploded/api/v1/notes/4f8a0a67-2ebc-41b2-9de6-4e9bcdba65bb
     public NoteDTO getNote(@PathVariable ("noteId") String noteId)  {
         System.out.println(noteId);
         return new NoteDTO(
@@ -47,14 +47,14 @@ public class NoteController {
     }
 
     //Todo: UPDATE a note
-    @PatchMapping(value = "/{noteId}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "/{noteId}",produces = MediaType.APPLICATION_JSON_VALUE)  // http://localhost:8080/NoteTaker_war_exploded/api/v1/notes/4f8a0a67-2ebc-41b2-9de6-4e9bcdba65bb
     public void updateNote(@PathVariable ("noteId") String noteId, @RequestBody NoteDTO note) {
         System.out.println(noteId);
         System.out.println(note+ " Updated");
     }
 
     //Todo: DELETE a note
-    @DeleteMapping(value ="/{noteId}" )
+    @DeleteMapping(value ="/{noteId}" ) //  http://localhost:8080/NoteTaker_war_exploded/api/v1/notes/1990119
     public void deleteNote(@PathVariable ("noteId") String noteId) {
         System.out.println(noteId + " Deleted");
     }
